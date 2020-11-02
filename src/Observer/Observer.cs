146aab1c0c9fe2055dde851101bb6458949fa08c
Ville Penttinen <villem.penttinen@gmail.com>
2020-11-02 16:44:06 +0200

    Try --force-recreate and don't clear out log file

--- src/Observer/Observer.cs
+++ src/Observer/Observer.cs
@@ -51,9 +51,8 @@ namespace Observer
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            logger.LogInformation("Clearing file {path}", settings.OutFilePath);
-
-            System.IO.File.WriteAllText(settings.OutFilePath, "");
+            // logger.LogInformation("Clearing file {path}", settings.OutFilePath);
+            // System.IO.File.WriteAllText(settings.OutFilePath, "");
 
             await ProgramCommon.WaitForRabbitMQ(stoppingToken);
 
