d0d7f33d0748a63a7d01fd42253b487f9423ed92
Ville Penttinen <villem.penttinen@gmail.com>
2020-11-28 01:49:40 +0200

    Refactor ServerMiddleware

--- src/HttpServer/Program.cs
+++ src/HttpServer/Program.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,32 +15,6 @@ namespace HttpServer
     public class Settings
     {
         public string OutFilePath { get; set; } = "";
-    }
-
-    public class ServerMiddleware
-    {
-        public ServerMiddleware(RequestDelegate _)
-        {
-        }
-
-        public async Task Invoke(HttpContext httpContext, IFileSystem fileSystem, ILogger<ServerMiddleware> logger, IConfiguration config)
-        {
-            var req = httpContext.Request;
-            logger.LogInformation("Received request: {@Headers} {@Path}", req.Headers, req.Path);
-
-            var settings = config.Get<Settings>();
-
-            string content = "";
-
-            try {
-                content = await fileSystem.File.ReadAllTextAsync(settings.OutFilePath, System.Text.Encoding.UTF8);
-                logger.LogInformation("Read from {path}: '{content}'", settings.OutFilePath, content.Replace(Environment.NewLine, "\\n"));
-            } catch (System.IO.FileNotFoundException ex) {
-                logger.LogWarning("File {path} does not exist.\n{@Exception}", settings.OutFilePath, ex);
-            }
-
-            await httpContext.Response.WriteAsync(content);
-        }
     }
 
     public class Program
---
+++ src/HttpServer/ServerMiddleware.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO.Abstractions;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    public class ServerMiddleware : IMiddleware
+    {
+        private readonly IFileSystem fileSystem;
+        private readonly ILogger<ServerMiddleware> logger;
+        private readonly IConfiguration config;
+        public ServerMiddleware(IFileSystem fileSystem, ILogger<ServerMiddleware> logger, IConfiguration config)
+        {
+            this.fileSystem = fileSystem;
+            this.logger = logger;
+            this.config = config;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, RequestDelegate _)
+        {
+            var req = httpContext.Request;
+            logger.LogInformation("Received request: {@Headers} {@Path}", req.Headers, req.Path);
+
+            var settings = config.Get<Settings>();
+
+            string content = "";
+
+            try
+            {
+                content = await fileSystem.File.ReadAllTextAsync(settings.OutFilePath, System.Text.Encoding.UTF8);
+                logger.LogInformation("Read from {path}: '{content}'", settings.OutFilePath, content.Replace(Environment.NewLine, "\\n"));
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                logger.LogWarning("File {path} does not exist.\n{@Exception}", settings.OutFilePath, ex);
+            }
+
+            await httpContext.Response.WriteAsync(content);
+        }
+    }
+}
