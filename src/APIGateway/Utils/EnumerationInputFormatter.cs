5eed920b8cf5a8363bcfb982b439a8fa9393759d
Ville Penttinen <villem.penttinen@gmail.com>
2020-11-28 14:48:29 +0200

    Remove unnecessary code

--- src/APIGateway/Utils/EnumerationInputFormatter.cs
+++ src/APIGateway/Utils/EnumerationInputFormatter.cs
@@ -1,10 +1,7 @@
-﻿using APIGateway.Features.States;
-using Common;
-using Microsoft.AspNetCore.Http;
+﻿using Common;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Net.Http.Headers;
 using System;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,51 +28,9 @@ namespace APIGateway.Utils
 
             var result = Enumeration.FromName(type, data);
 
-            //var readerType = typeof(EnumerationReader<>).MakeGenericType(type);
-
-            //var readerInstance = Activator.CreateInstance(
-            //    typeof(EnumerationReader<>).MakeGenericType(
-            //        new Type[] { type }),
-            //    BindingFlags.Instance | BindingFlags.Public,
-            //    binder: null,
-            //    args: Array.Empty<object>(),
-            //    culture: null) as EnumerationReader;
-
-            //var value = readerInstance?.Read(data);
-
-            //if (value is null)
-            //{
-            //    throw new InvalidOperationException($"Unable to read '{data}' as {type}");
-            //}
-
             return InputFormatterResult.Success(result);
         }
 
         protected override bool CanReadType(Type type) => type.IsImplementationOf(typeof(Enumeration));
-
-        private abstract class EnumerationReader
-        {
-            public abstract object Read(string data);
-        }
-
-        private class EnumerationReader<T> : EnumerationReader
-            where T : Enumeration
-        {
-            public override object Read(string data)
-            {
-                return Enumeration.FromName<T>(data);
-            }
-        }
-
-        //public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
-
-        //{
-        //    if (context.Object is not ApplicationState state)
-        //    {
-        //        throw new InvalidOperationException($"Invalid type {context.ObjectType}");
-        //    }
-
-        //    await context.HttpContext.Response.WriteAsync(state.ToString());
-        //}
     }
 }
