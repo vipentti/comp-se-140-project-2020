43575f48a4a5261aaa442d96545c483426c2ee8b
Ville Penttinen <villem.penttinen@gmail.com>
2020-12-01 15:55:51 +0200

    Implement new TopicMessage type for Observer messages

---
+++ src/Common/Messages/TopicMessage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Common.Messages
+{
+    public record TopicMessage(DateTime Timestamp, string Topic, string Content)
+    {
+        private const string TopicPrefix = "Topic ";
+        private const string ContentPrefix = ": ";
+
+        public static TopicMessage FromString(string input)
+        {
+            var indexOfTopic = input.IndexOf(TopicPrefix);
+
+            if (indexOfTopic == -1)
+            {
+                throw new InvalidOperationException($"'{input}' is not valid {nameof(TopicMessage)}");
+            }
+
+            var indexOfContent = input.IndexOf(ContentPrefix, indexOfTopic + 1);
+
+            if (indexOfContent == -1)
+            {
+                throw new InvalidOperationException($"'{input}' is not valid {nameof(TopicMessage)}");
+            }
+
+            var timestamp = input[0..indexOfTopic].Trim();
+
+            var datetime = timestamp.FromISO8601() ?? throw new InvalidOperationException($"Invalid datetime {timestamp}");
+
+            var topicStart = indexOfTopic + TopicPrefix.Length;
+
+            var topic = input[topicStart..indexOfContent];
+
+            var contentStart = indexOfContent + ContentPrefix.Length;
+
+            // There should always be a whitespace between colon and the message
+            var content = input[contentStart..];
+
+            return new TopicMessage(
+                datetime,
+                topic,
+                content
+            );
+        }
+
+        public override string ToString() => $"{Timestamp.ToISO8601()} Topic {Topic}: {Content}";
+    }
+}
---
+++ tests/Unit.Tests/TopicMessageTests.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using Common.Messages;
+using FluentAssertions;
+using Xunit;
+
+namespace Unit.Tests
+{
+    public class TopicMessageTests
+    {
+        [Theory]
+        [ClassData(typeof(Topics))]
+        public void TopicMessage_Supports_RoundTrip_Parsing(TopicMessage topic)
+        {
+            var asString = topic.ToString();
+
+            asString.Should().Be($"{topic.Timestamp.ToISO8601()} Topic {topic.Topic}: {topic.Content}");
+
+            var fromString = TopicMessage.FromString(asString);
+
+            fromString.Should().Be(topic);
+        }
+
+        internal class Topics : TheoryData<TopicMessage>
+        {
+            public Topics()
+            {
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), "test-topic", "test-message"));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), "test-topic with whitespace after ", " test-message with withspaces "));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), " test-topic with whitespace before and after ", "   test-message with withspaces   "));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), "topic with empty content", ""));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), "", "content with empty topic"));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), "", ""));
+                Add(new TopicMessage(TestUtils.Utils.GetDefaultTestTime(), " ", " "));
+            }
+        }
+    }
+}
