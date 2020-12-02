edc8a6587a3b0858dd163f0c1d47b57cf833e129
Ville Penttinen <villem.penttinen@gmail.com>
2020-12-02 16:19:19 +0200

    Add initial GET /node-statistic to pass first set of tests

---
+++ src/APIGateway/Features/Statistics/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace APIGateway.Features.Statistics
+{
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/node-statistic")]
+        public async Task<ActionResult<string>> GetNodeStatistics()
+        {
+            await Task.Delay(0);
+            return "";
+        }
+    }
+}
